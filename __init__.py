d31ff32e0743fd6fc662f2136d5f43fd34b41e5b
Conelius Kavoo <kavoocornelius7@gmail.com>
2026-06-10 19:42:05 +0300

    Add package initialization file for better module discovery

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# Task Management Package
+from .task_utils import add_task, mark_task_as_complete, view_pending_tasks, calculate_progress, tasks
+
+__all__ = ['add_task', 'mark_task_as_complete', 'view_pending_tasks', 'calculate_progress', 'tasks']
