bace6dc0dba5c4e9ef26439bc44d67fbcbb9af75
Conelius Kavoo <kavoocornelius7@gmail.com>
2026-06-10 20:13:35 +0300

    Fixed codegrade issues

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+
 from task_utils import add_task, mark_task_as_complete, view_pending_tasks, calculate_progress, tasks
 
 # Define the main function
